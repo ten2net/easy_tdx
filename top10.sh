4cff0b5dd1880b26304930450784e14bc95aa801
ten2net <ten2net@163.com>
2026-07-07 17:13:03 +0800

    feat: add top10.sh script for screen strength analysis with specified parameters

---
+++ top10.sh
@@ -0,0 +1,2 @@
+export TDX_HOME="./new_tdx"
+easy-tdx screen strength --universe sz --preset breakout  --top 50 --min-amount 1000000000 --workers 8 --table --names --output sz_strength_20260706_breakout.json
